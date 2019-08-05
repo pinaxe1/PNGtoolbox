cfeaaffb6eca4a25da4f5bf32244390196aaa87e
pinaxe1 <pinaxe@gmail.com>
2019-08-05 11:16:48 -0400

    Add files via upload

--- PNG-ReMask.py
+++ PNG-ReMask.py
@@ -22,11 +22,11 @@ Allowed Masks are
 """
 ExpectedLClasses={0, 33, 6, 7, 27, 21} # Classes I currently use in the dataset for FCN-128 
 
-sourceList={33, 27, 21}# Classes to be "renamed" so they wouldn't exceed NUmClasses in my FCN-128 
-dstList={8,9,10}       # A new "names" for the classes
+sourceList=[33, 255, 27, 21]# Classes to be "renamed" so they wouldn't exceed NUmClasses in my FCN-128 
+dstList   =[8 , 0  , 9 , 10]# A new "names" for the classes
 
-mypath="C:/D/notebooks/fcn-mobilenet-128/Data_zoo/camvid/3/"
-destpath="C:/D/notebooks/fcn-mobilenet-128/Data_zoo/camvid/1/"
+mypath  ="C:/D/notebooks/fcn-mobilenet-128/Data_zoo/camvid/1/"
+destpath="C:/D/notebooks/fcn-mobilenet-128/Data_zoo/camvid/3/"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 for fi in onlyfiles :
     dst=cv2.imread(mypath+fi) # CV2.read  always converts image to RGB.
